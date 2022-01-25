d4ca962bf81fb7a3795cff7efe950dc5493603ad
BaxtiyorMurodov <baxtiyormurodovv@gmail.com>
2022-01-25 13:59:30 -0800

    some changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './pages/home/Home';
 import Flat from './pages/flat/Flat';
 import { Routes, Route } from "react-router-dom";
 
-
 class App extends React.PureComponent {
   render() {
     return (
