acebf59758c70d30962535ce996803742d56f00c
BaxtiyorMurodov <baxtiyormurodovv@gmail.com>
2022-01-25 13:07:08 -0800

    a lot of changes

--- src/components/mainWork/MainWork.js
+++ src/components/mainWork/MainWork.js
@@ -1,30 +1,29 @@
-import React from 'react'
-import './MainWork.css'
-import Photo from './work-main.png'
+import React from "react";
+import "./MainWork.css";
+import Photo from "./work-main.png";
 
-import CardTop from '../main-card/Card-top';
-import MainWorkContent from './mainWorkContent/MainWorkContent';
+import CardTop from "../main-card/Card-top";
+import MainWorkContent from "./mainWorkContent/MainWorkContent";
+
 class MainWork extends React.Component {
-    render() { 
-        return (
-          <section>
-            <div className="container  " >  
-                <CardTop cardTitle={"Ishlar"} cartText={"MUSOFIRLAR.UZ kascasbcjkascas "} />
-                <div className="main-ish-bottom">
-                  <MainWorkContent />
-                  <div>
-                    <img
-                      src={Photo}
-                    //   alt="main ish rasmi"
-                      width="500px"
-                      height="550px"
-                    />
-                  </div>
-                </div>
-              </div>
-          </section>
-        );
-    }
+  render() {
+    return (
+      <section>
+        <div className="container  ">
+          <CardTop
+            cardTitle={"Ishlar"}
+            cartText={"MUSOFIRLAR.UZ kascasbcjkascas "}
+          />
+          <div className="main-ish-bottom">
+            <MainWorkContent />
+            <div>
+              <img src={Photo} width="500px" height="550px" />
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
- 
+
 export default MainWork;
