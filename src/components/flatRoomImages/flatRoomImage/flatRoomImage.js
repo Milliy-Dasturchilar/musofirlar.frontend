b174268b500c5df1a92687f124a43f750a318816
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-01-26 14:37:30 +0500

    added flat room card

--- src/components/flatRoomImages/flatRoomImage/flatRoomImage.js
+++ src/components/flatRoomImages/flatRoomImage/flatRoomImage.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react";
 import "./flatRoomImage.css";
+// import "./../../main-card/MainCard.css"
 import Photo from "./roomImage.png";
 import Icon from "../like-icon.svg";
 class FlatRoomImage extends PureComponent {
@@ -14,12 +15,34 @@ class FlatRoomImage extends PureComponent {
                 <div className="room-image-title-container">
                     <div className="room-image-title">
                         <div className="flat-room-container">
-                            <h4 className="flat-price">2 Xonali / 80m2</h4>
-                            <img src={Icon} />
+                            <h4 className="flat-rooms" >2 Xonali / 80m2</h4>
+                            <img src={Icon} className="flat-like-icon"/>
                         </div>
-                        {/* <h4></h4> */}
-                    </div>
+                        <h4 className="flat-price">150.000so’m</h4>
+                        <div className="room-equipment-container">
+                            <div className="room-equipment-title">
+                                <i className="fas fa-map-marker-alt"></i>
+                                <span>London</span>
+                            </div>
+                            <div className="room-equipment-title">
+                                <i className="fas fa-user-circle"></i>
+                                <span>Yakkalik</span>
+                            </div>
+                        </div>
+                        <div className="room-equipment-container">
+                            <div className="room-equipment-title">
+                                <i className="fas fa-chair"></i>
+                                <span>Mebellar</span>
+                            </div>
+                            <div className="room-equipment-title">
+                                <i className="fas fa-dollar-sign"></i>
+                                <span>Arzon</span>
+                            </div>
+                        </div>
+                        
 
+                    </div>
+                      <button className="flat-about-btn">Batafsil</button>
                 </div>
             </div>
 
