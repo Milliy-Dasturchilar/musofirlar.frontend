fcf652961fac864261e0a0af5ad2a7d564400936
ulugbekmahammadjonov <ulugbekmahammadjonov1218@gmail.com>
2022-01-28 21:44:56 +0500

    added the first card of job componend

--- src/components/jobImages/jobImage/JobImage.js
+++ src/components/jobImages/jobImage/JobImage.js
@@ -15,31 +15,36 @@ class JobImage extends React.Component {
                 </div>
                 <p className="img-text-new">Yangi</p>
                 <div className="job-card-title-container">
-                     <h4 className="job-card-title">Mohir ustalarni ishga taklif etamiz
-                     7.500.000so’m gacha oylik maosh</h4>
-                     <p className="job-card-text">Afzalliklari:</p>
-                     <div className="job-equipment-container">
-                            <div className="job-equipment-title">
-                                <i className="fas fa-map-marker-alt"></i>
-                                <span>London</span>
-                            </div>
-                            <div className="job-equipment-title">
-                                <i className="fas fa-user-circle"></i>
-                                <span>Yakkalik</span>
-                            </div>
+                    <h4 className="job-card-title">Mohir ustalarni ishga taklif etamiz
+                        <span> 7.500.000</span><p className="sum">so'm</p> gacha oylik maosh</h4>
+                    <p className="job-card-text">Afzalliklari:</p>
+                    <div className="job-equipment-container">
+                        <div className="job-equipment-title">
+                            <i className="fas fa-map-marker-alt"></i>
+                            <span>London</span>
                         </div>
-                        <div className="job-equipment-container">
-                            <div className="job-equipment-title">
-                                <i className="fas fa-chair"></i>
-                                <span>Mebellar</span>
-                            </div>
-                            <div className="job-equipment-titl">
-                                <i className="fas fa-dollar-sign"></i>
-                                <span>Arzon</span>
-                            </div>
+                        <div className="job-equipment-title">
+                            <i class="far fa-clock"></i>
+                            <span>8 soatlik ish</span>
                         </div>
+                    </div>
+                    <div className="job-equipment-container">
+                        <div className="job-equipment-title">
+                            <i class="fas fa-couch" style={{ fontSize: '10px' }}></i>
+                            <span>Yotoq</span>
+                        </div>
+                        <div className="job-equipment-title">
+                            <i class="fas fa-utensils"></i>
+                            <span>3 mahal ovqat</span>
+                        </div>
+                    </div>
+                    <div className="job-container-btn">
+                        <button className="contact-job-btn">Bog'lanish</button>
+                        <button className="detailed-job-btn">Batafsil</button>
+                    </div>
 
                 </div>
+
             </div>
 
         );
